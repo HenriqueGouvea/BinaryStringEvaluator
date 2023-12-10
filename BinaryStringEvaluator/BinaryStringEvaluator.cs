9d2fca78dc368ea1a8c295fbc4c97d62e5382520
Henrique Gouvea <henriquegouvea90@gmail.com>
2023-12-10 00:36:56 +0000

    Update readme

--- BinaryStringEvaluator/BinaryStringEvaluator.cs
+++ BinaryStringEvaluator/BinaryStringEvaluator.cs
@@ -4,11 +4,6 @@
     {
         public static bool Evaluate(string binaryString)
         {
-            /* Write a C# function to evaluate binary strings based on specific criteria.
-               1.The function accepts a binary string as input.
-               2.Check if the binary string is 'good' based on these conditions:
-                ● Equal number of 0's and 1's.
-                ● For every prefix, the number of 1's is not less (higher or equals) than the number of 0's.*/
             if (string.IsNullOrWhiteSpace(binaryString))
                 return false;
 
